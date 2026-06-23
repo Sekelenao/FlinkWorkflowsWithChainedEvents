9346c5461ed1e98f29a8a56161ab12fc0699543b
Sekelenao <Sekelenao@protonmail.com>
2026-06-24 01:21:54 +0200

    dummy code in delay + scripts to launch/remove a job

--- DelayWorkflow/src/main/java/io/github/sekelenao/delay/Main.java
+++ DelayWorkflow/src/main/java/io/github/sekelenao/delay/Main.java
@@ -1,9 +1,37 @@
 package io.github.sekelenao.delay;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.legacy.SourceFunction;
+
 public final class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.addSource(new DummySource())
+           .name("dummy-source")
+           .print()
+           .name("dummy-print");
+
+        env.execute("DelayWorkflow");
     }
 
+    private static class DummySource implements SourceFunction<String> {
+        private volatile boolean isRunning = true;
+
+        @Override
+        public void run(SourceContext<String> ctx) throws Exception {
+            long count = 0;
+            while (isRunning) {
+                ctx.collect("Dummy event " + count++);
+                Thread.sleep(1000);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            isRunning = false;
+        }
+    }
 }
+
