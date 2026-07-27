e750a51333eeea068766c599066807f505f6ba4c
Sekelenao <Sekelenao@protonmail.com>
2026-07-28 00:08:01 +0200

    feat(delay-workflow): add Kafka sink configuration, serialization schema, and relocate Jackson shaded classes

--- DelayWorkflow/src/main/java/io/github/sekelenao/delay/Main.java
+++ DelayWorkflow/src/main/java/io/github/sekelenao/delay/Main.java
@@ -1,26 +1,35 @@
 package io.github.sekelenao.delay;
 
-import io.github.sekelenao.delay.configuration.JobProperties;
+import io.github.sekelenao.delay.configuration.JobConfiguration;
+import io.github.sekelenao.delay.event.TrainEvent;
 import io.github.sekelenao.delay.kafka.TrainEventKafkaDeserializer;
+import io.github.sekelenao.delay.kafka.TrainEventSerializationSchema;
 import io.github.sekelenao.delay.operator.DumbMapFunction;
 import io.github.sekelenao.flinkboot.core.api.Flinkboot;
+import io.github.sekelenao.flinkboot.kafka.api.sink.KafkaSinkFactory;
 import io.github.sekelenao.flinkboot.kafka.api.source.KafkaSourceFactory;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 
 public final class Main {
 
     public static void main(String[] args) throws Exception {
         var flinkboot = Flinkboot.initialize(args);
-        var configuration = flinkboot.configuration(JobProperties.class);
+        var configuration = flinkboot.configuration(JobConfiguration.class);
         var executionEnvironment = flinkboot.executionEnvironment(configuration.job());
+
         var source = KafkaSourceFactory.supplyFor(configuration.kafka(), new TrainEventKafkaDeserializer());
-        var stream = executionEnvironment.fromSource(
-            source,
-            WatermarkStrategy.noWatermarks(),
-            "Kafka Train Events Source"
-        );
-        stream.map(new DumbMapFunction(configuration.job().name())).print();
+
+        var serializationSchema = KafkaRecordSerializationSchema.<TrainEvent>builder()
+                .setTopic(configuration.kafkaSink().topic())
+                .setValueSerializationSchema(new TrainEventSerializationSchema())
+                .build();
+        var sink = KafkaSinkFactory.supplyFor(configuration.kafkaSink(), serializationSchema);
+
+        var stream = executionEnvironment.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Train Events Source");
+        var mappedStream = stream.map(new DumbMapFunction(configuration.job().name()));
+        mappedStream.sinkTo(sink);
+
         executionEnvironment.execute("DelayWorkflow");
     }
 
